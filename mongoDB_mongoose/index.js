f9db7d2435392e92200779618eb9f88c0ad97b72
mazumdaratik <rjatik990@gmail.com>
2025-01-28 20:43:20 +0600

    connecting mongoDB

---
+++ mongoDB_mongoose/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const todoHandler = require('./routeHandler/todoHandler');
+const app = express();
+
+app.use(express.json());
+
+//database conncetion with mongoose
+mongoose
+    .connect('mongodb://localhost/todos', {useNewUrlParser: true, useUnifiedTopology: true,})
+    .then( () => console.log('connection Succesfull'))
+    .catch((err) => console.log(err))
+
+//application routes
+app.use('/todo', todoHandler);
+
+//default error handling
+function errorHandler(err, req, res, next){
+    if(res.headersSent){
+        return next(err);
+    };
+    res.status(500).json({error: err});
+}
+
+app.listen(3000, () => {
+    console.log('app listening on port 3000');
+});
