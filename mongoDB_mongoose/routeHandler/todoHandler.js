5c9a3d9dfe6185d934a4f78cb58f27a53f0063b3
mazumdaratik <rjatik990@gmail.com>
2025-01-28 20:44:40 +0600

    all the routes created

---
+++ mongoDB_mongoose/routeHandler/todoHandler.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+
+//Get all the todos
+router.get('/', async (req, res) => {
+
+});
+//Get a todo by ID
+router.get('/:id', async (req, res) => {
+
+});
+//Post a Todo
+router.post('/', async (req, res) => {
+
+});
+//Post multiple Todo
+router.post('/all', async (req, res) => {
+
+});
+
+//Put a todo
+router.put('/:id', async (req, res) => {
+
+});
+
+//delete a TODO
+router.delete('/:id', async (req, res) => {
+
+});
+
+module.exports = router;
